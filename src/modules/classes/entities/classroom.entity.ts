ffe4dc29d0c27e7f0a1935bed724bb9ac279745c
Jordy Sarmiento <jordysarmiento@gmail.com>
2025-12-12 12:54:22 -0500

    feat: entidades classroom y class

--- src/modules/classes/entities/classroom.entity.ts
+++ src/modules/classes/entities/classroom.entity.ts
@@ -6,8 +6,14 @@ import {
   UpdateDateColumn,
   DeleteDateColumn,
   ManyToOne,
+  ManyToMany,
+  JoinTable,
+  OneToMany,
+  Index,
 } from 'typeorm';
 import { Class } from './class.entity';
+import { School } from 'src/modules/schools/entities/school.entity';
+import { SchoolUser } from 'src/modules/schools/entities/school-user.entity';
 
 @Entity()
 export class Classroom {
@@ -15,7 +21,30 @@ export class Classroom {
   classroomId: number;
 
   @Column({ length: 100 })
+  @Index()
   name: string;
+
+  @Column({ length: 10 })
+  @Index()
+  grade: string;
+
+  @Column({ length: 5 })
+  @Index()
+  section: string;
+
+  @ManyToOne(() => School, (school) => school.classrooms)
+  school: School;
+
+  @ManyToMany(() => SchoolUser)
+  @JoinTable({ name: 'classroom_students' })
+  students: SchoolUser[];
+
+  @ManyToMany(() => SchoolUser)
+  @JoinTable({ name: 'classroom_teachers' })
+  teachers: SchoolUser[];
+
+  @OneToMany(() => Class, (cls) => cls.classroom)
+  classes: Class[];
 
   @CreateDateColumn()
   createdAt: Date;
@@ -25,7 +54,4 @@ export class Classroom {
 
   @DeleteDateColumn()
   deletedAt: Date;
-
-  @ManyToOne(() => Class, (cls) => cls.classrooms)
-  class: Class;
 }
